2f76265f7eca68e0260ec60c26e6a4428e4cfae9
OsmonovMeder08 <osmonovmeder748@gmail.com>
2025-07-03 17:42:32 +0600

    i change my logo

--- src/data/reciters.ts
+++ src/data/reciters.ts
@@ -3,11 +3,11 @@ import { Reciter } from '../types';
 export const reciters: Reciter[] = [
   {
     id: 1,
-    name: 'Ahmed ibn Ali al-Ajamy',
-    arabicName: 'أحمد بن علي العجمي',
-    image: 'https://assets.audiomack.com/theholyqurantv/1c0a880a117abb6ec754a76b0020a1f6bbcf87338ddae111c8876787eeba6277.jpeg?width=1000&height=1000&max=true',
-    followers: '2.1M',
+    name: 'Osmonov Meder',
+    arabicName: 'عثمانوف ميدر',
+    image: 'https://i.postimg.cc/qMKyTFm4/meda.jpg',
+    followers: '7.1M',
     description: 'Beautiful melodic recitation',
-    country: 'Saudi Arabia'
+    country: 'Kyrgyzstan'
   }
 ];
